132c491c3677e10cde21428f1d7968e36b8ca2e4
DJAFFF <muhamaddjafar64@gmail.com>
2024-11-04 09:06:02 +0700

    Add files via upload

---
+++ Latihan_18.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan.pkg5;
+
+/**
+ *
+ * @author USER
+ */
+public class Latihan_18 {
+  
+    public static void main(String[] args) {
+        double[] output = {3.5, 7.0, 10.5, 14.0, 17.5, 21.0, 24.5, 28.0, 31.5, 35.0};
+
+        System.out.println("Output Values:");
+        for (double value : output) {
+            System.out.println(value);
+        }
+
+        System.out.println("\nBUILD");
+    }
+}
+
